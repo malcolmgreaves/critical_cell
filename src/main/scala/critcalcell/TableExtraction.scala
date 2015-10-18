b39857ff6ac36484c5054fb2bc583c0393dfc3e2
Malcolm Greaves <greaves.malcolm@gmail.com>
2015-10-18 00:20:54 -0700

    Re-run over all comments. Improved readability of numberOfCells

--- src/main/scala/critcalcell/TableExtraction.scala
+++ src/main/scala/critcalcell/TableExtraction.scala
@@ -18,8 +18,8 @@ object TableExtraction {
    * Every cell has its row and column indicies.
    */
   sealed trait Cell {
-    def rawRow: Int
-    def rawCol: Int
+    def row: Int
+    def col: Int
   }
   object Cell {
     /** Converts a cell into a string representation. */
@@ -31,13 +31,13 @@ object TableExtraction {
   }
 
   /** Cell implementation when its content is a string. */
-  case class Str(content: String, rawRow: Int, rawCol: Int) extends Cell
+  case class Str(content: String, row: Int, col: Int) extends Cell
 
   /** Cell implementation when its content is a real-valued number. */
-  case class Dbl(content: Double, rawRow: Int, rawCol: Int) extends Cell
+  case class Dbl(content: Double, row: Int, col: Int) extends Cell
 
   /** Cell instance for something that contains no content. */
-  case class Empty(rawRow: Int, rawCol: Int) extends Cell
+  case class Empty(row: Int, col: Int) extends Cell
   object Empty {
     /** An empty cell with row and column indicies equal to 0. */
     val zero = Empty(0, 0)
@@ -50,34 +50,37 @@ object TableExtraction {
     val default = Start(row = 0, col = 0)
   }
 
-  /** Type documentation for number of cells function. */
+  /** Type documentation for numberOfCells function. */
   type IsRow = Boolean
 
   /**
-   * Function for finding the number of cells in the table on a particular axis.
+   * Function for finding the number of cells in the table on a given axis.
    * numberOfCells iterates through the table, looking at cells that are in a
    * particular row (or column if IsRow=false). The evaluated Int is the number
    * of cells found in the table's row (or column).
    */
   val numberOfCells: Table => IsRow => Start => Int =
-    table => isRow => start =>
-      if (isRow)
-        table.foldLeft(0) {
-          (sameRowOrCol, cell) =>
-            if (cell.rawRow == start.row && cell.rawCol >= start.col)
-              sameRowOrCol + 1
-            else
-              sameRowOrCol
-        }
+    table => isRow => start => {
 
-      else
-        table.foldLeft(0) {
-          (sameRowOrCol, cell) =>
-            if (cell.rawRow >= start.row && cell.rawCol == start.col)
-              sameRowOrCol + 1
+      val shouldIncrement: Cell => Boolean =
+        if (isRow)
+          // cell in same row, any column right of start
+          cell =>
+            cell.row == start.row && cell.col >= start.col
+        else
+          // cell in same column, any row above start
+          cell =>
+            cell.col == start.col && cell.row >= start.row
+
+      table
+        .foldLeft(0) {
+          (trace, cell) =>
+            if (shouldIncrement(cell))
+              trace + 1
             else
-              sameRowOrCol
+              trace
         }
+    }
 
   /** Algorithm configuration: upper bound on row and column during search. */
   case class Max(row: Int, col: Int)
@@ -96,7 +99,8 @@ object TableExtraction {
   /** FindCriticalCell function using the default configuration. */
   lazy val findCritical: FindCriticalCell =
     // Note the "lazy val" because we use "findCriticalUsing" before it is
-    // defined. The lazy modifier delays initialization until absolutely needed.
+    // defined.
+    // The lazy modifier delays initialization until absolutely needed.
     findCriticalUsing(Conf.default)
 
   /**
@@ -107,7 +111,7 @@ object TableExtraction {
   val findCriticalUsing: Conf => FindCriticalCell =
     conf => table => {
 
-        /** Method for finding the critical cell inidices. */
+        // Helper method for finding the critical cell inidices.
         def findIndex(
           numCells: Start => Int,
           max:      Int,
@@ -122,14 +126,8 @@ object TableExtraction {
             }
             .getOrElse(max)
 
-      // find the 1st row, starting from row 1,
-      // s.t. the # of cells in the row with column # >= 1
-      // is equivalent to the # of cells in the previous row
-      // (with the same column constraint)
-      //
-      // so, for instance, we're never going to consider any
-      // cells that exist to the left of the first column
-      // when doing this counting
+      // Find 1st row such that the # of cells in the row
+      // is equivalent to the # of cells in the previous row.
       val rowIndexP =
         findIndex(
           numberOfCells(table)(true),
@@ -138,22 +136,23 @@ object TableExtraction {
           (s: Start, newRow: Int) => s.copy(row = newRow)
         )
 
-      // ditto for column
-      // We use the found row index (rowIndexP) as the new starting row.
+      // Find 1st column such that the # of cells in the column
+      // is equivalent to the # of cells in the previous column.
       val colIndexP =
         findIndex(
           numberOfCells(table)(false),
           conf.max.col,
-          conf.start
-            .copy(row = rowIndexP),
+          // We use the found row index (rowIndexP) as the new starting row.
+          conf.start.copy(row = rowIndexP),
           (s: Start, newCol: Int) => s.copy(col = newCol)
         )
 
       table
-        // locate the critical cell using these found (row,col) coordinates
-        // if no such cell matches, then we evaluate to None
+        // Locate the critical cell using these found (row,col) coordinates.
+        // If no such cell matches, then the table has no critical cell.
+        // Indicated by evaluating to None here.
         .find { cell =>
-          cell.rawRow == rowIndexP && cell.rawCol == colIndexP
+          cell.row == rowIndexP && cell.col == colIndexP
         }
     }
 }
